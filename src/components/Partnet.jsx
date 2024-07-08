364c261816c6100382ed7861d9af52c18bf541aa
getch-hasan <hasan15-3112@diu.edu.bd>
2024-07-08 15:50:29 +0600

    feat:lamding section

--- src/components/Partnet.jsx
+++ src/components/Partnet.jsx
@@ -5,7 +5,7 @@ const Partner = () => {
     return (
         <div>
             <div className='grid justify-center'>
-                <h1 className='text-center text-xl md:text-5xl font-bold py-10' >Be Our Partner/Reseller</h1>
+                <h1 className='text-center text-xl md:text-5xl font-bold py-10' >Be Our Partner or Reseller</h1>
             </div>
             <div className='grid justify-center items-center'>
                 <Image className='w-[1000px] p-10' height={1000} width={1000} src={"/image/Partner.webp"} />
