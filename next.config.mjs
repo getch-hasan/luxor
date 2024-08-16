260a4af8a659dc0a32f3ceb4784ccfd165e646d0
getch-hasan <hasan15-3112@diu.edu.bd>
2024-08-16 21:38:37 +0600

    fix error

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-import { withNextVideo } from "next-video/process";
+// import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  output:"export"
 };
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
